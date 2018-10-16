e4597ab4209a8af59e0dd8c48f0d56144b3fd995
Alex Basson <alexbasson@gmail.com>
2018-10-16 05:48:12 -0400

    Input/output numbers as strings instead of arrays

--- spec/solution.spec.js
+++ spec/solution.spec.js
@@ -66,54 +66,70 @@ const flipBit = (bit) => {
   return bit === 0 ? 1 : 0
 }
 
+const toArray = (number) => {
+  return number.split('').map((digit) => parseInt(digit))
+}
+
+const toString = (numArray) => {
+  return numArray.join('')
+}
+
 const flipDigit = (number, position) => {
-  const bits = Math.log2(number.length)
-  const encoded = encode(number)
+  const numArray = toArray(number)
+  const bits = Math.log2(numArray.length)
+  const encoded = encode(numArray)
   const positionInBinary = toBinary(position, bits)
   const difference = diff(encoded, positionInBinary)
   const positionToFlip = number.length - toDecimal(difference) - 1
-  number[positionToFlip] = flipBit(number[positionToFlip])
-  return number
+  numArray[positionToFlip] = flipBit(numArray[positionToFlip])
+  return toString(numArray)
 }
 
 const findPosition = (number) => {
-  return toDecimal(encode(number))
-}
-
-const printLongNumber = (number) => {
-  for (let i = 0; i < number.length; i++) {
-    console.log('index:', i, 'digit:', number[i])
-  }
-  console.log('=================')
+  return toDecimal(encode(toArray(number)))
 }
 
 const annotatedFlipDigit = (number, position) => {
-  console.log('original number:')
-  printLongNumber(number)
+  console.log('original number:', number)
+  console.log('position to find:', position)
 
-  console.log('position to find', position)
+  const whitespace = (n) => {
+    const spaces = []
+    for (let i = 0; i < n; i++) {
+      spaces.push(' ')
+    }
+    return spaces.join('')
+  }
 
-  const bits = Math.log2(number.length)
-  console.log('bits', bits)
+  const numArray = toArray(number)
 
-  const encoded = encode(number)
-  console.log('encoded', encoded)
+  const bits = Math.log2(numArray.length)
+  console.log('encoded bit size:', bits)
+  console.log('')
+
+  const encoded = encode(numArray)
+  console.log('              original number, encoded:', encoded)
 
   const positionInBinary = toBinary(position, bits)
-  console.log('position in binary', positionInBinary)
+  console.log('           position to find, in binary:', positionInBinary)
 
   const difference = diff(encoded, positionInBinary)
-  console.log('diff b/w encoded original and position', difference)
+  console.log('diff b/w encoded original and position:', difference)
 
-  const positionToFlip = number.length - toDecimal(difference) - 1
-  console.log('position to flip', positionToFlip)
+  console.log('')
 
-  number[positionToFlip] = flipBit(number[positionToFlip])
+  const positionToFlip = numArray.length - toDecimal(difference) - 1
 
-  return number
+  console.log('original number:', toString(numArray))
+  numArray[positionToFlip] = flipBit(numArray[positionToFlip])
+  console.log(' flipped number:', toString(numArray))
+  const padding = whitespace(positionToFlip)
+  console.log(padding + '                ' + ' ^')
+  console.log(padding + 'flip bit @ index', positionToFlip)
 
+  console.log('')
 
-  return toDecimal(encode(number))
+  return toString(numArray)
 }
 
 const testNumber = []
@@ -123,13 +139,13 @@ for (let i = 0; i < 64; i++) {
 
 const positionToFind = Math.round(63 * Math.random())
 
-const flippedNumber = annotatedFlipDigit(testNumber, positionToFind)
-console.log('flipped number:')
-printLongNumber(flippedNumber)
+
+const flippedNumber = annotatedFlipDigit(toString(testNumber), positionToFind)
 
 const foundPosition = findPosition(flippedNumber)
 console.log('position to find:', positionToFind)
 console.log('found position:', foundPosition)
+
 
 describe('xor', () => {
   it('combines an array of n-bit numbers and returns a new n-bit number', () => {
@@ -225,158 +241,96 @@ describe('encode', () => {
 
 describe('flipDigit', () => {
   it('finds the correct position', () => {
-    expect(findPosition(flipDigit([0, 0], 0))).toEqual(0)
-    expect(findPosition(flipDigit([0, 1], 0))).toEqual(0)
-    expect(findPosition(flipDigit([1, 0], 0))).toEqual(0)
-    expect(findPosition(flipDigit([1, 1], 0))).toEqual(0)
+    expect(findPosition(flipDigit('00', 0))).toEqual(0)
+    expect(findPosition(flipDigit('00', 1))).toEqual(1)
 
-    expect(findPosition(flipDigit([0, 0], 1))).toEqual(1)
-    expect(findPosition(flipDigit([0, 1], 1))).toEqual(1)
-    expect(findPosition(flipDigit([1, 0], 1))).toEqual(1)
-    expect(findPosition(flipDigit([1, 1], 1))).toEqual(1)
+    expect(findPosition(flipDigit('01', 0))).toEqual(0)
+    expect(findPosition(flipDigit('01', 1))).toEqual(1)
 
-    expect(findPosition(flipDigit([0, 0, 0, 0], 0))).toEqual(0)
-    expect(findPosition(flipDigit([0, 0, 0, 0], 1))).toEqual(1)
-    expect(findPosition(flipDigit([0, 0, 0, 0], 2))).toEqual(2)
-    expect(findPosition(flipDigit([0, 0, 0, 0], 3))).toEqual(3)
-    expect(findPosition(flipDigit([0, 0, 0, 0], 0))).toEqual(0)
-    expect(findPosition(flipDigit([0, 0, 0, 0], 1))).toEqual(1)
-    expect(findPosition(flipDigit([0, 0, 0, 0], 2))).toEqual(2)
-    expect(findPosition(flipDigit([0, 0, 0, 0], 3))).toEqual(3)
+    expect(findPosition(flipDigit('10', 0))).toEqual(0)
+    expect(findPosition(flipDigit('10', 1))).toEqual(1)
 
-    expect(findPosition(flipDigit([0, 0, 0, 1], 0))).toEqual(0)
-    expect(findPosition(flipDigit([0, 0, 0, 1], 1))).toEqual(1)
-    expect(findPosition(flipDigit([0, 0, 0, 1], 2))).toEqual(2)
-    expect(findPosition(flipDigit([0, 0, 0, 1], 3))).toEqual(3)
-    expect(findPosition(flipDigit([0, 0, 0, 1], 0))).toEqual(0)
-    expect(findPosition(flipDigit([0, 0, 0, 1], 1))).toEqual(1)
-    expect(findPosition(flipDigit([0, 0, 0, 1], 2))).toEqual(2)
-    expect(findPosition(flipDigit([0, 0, 0, 1], 3))).toEqual(3)
+    expect(findPosition(flipDigit('11', 0))).toEqual(0)
+    expect(findPosition(flipDigit('11', 1))).toEqual(1)
 
-    expect(findPosition(flipDigit([0, 0, 1, 0], 0))).toEqual(0)
-    expect(findPosition(flipDigit([0, 0, 1, 0], 1))).toEqual(1)
-    expect(findPosition(flipDigit([0, 0, 1, 0], 2))).toEqual(2)
-    expect(findPosition(flipDigit([0, 0, 1, 0], 3))).toEqual(3)
-    expect(findPosition(flipDigit([0, 0, 1, 0], 0))).toEqual(0)
-    expect(findPosition(flipDigit([0, 0, 1, 0], 1))).toEqual(1)
-    expect(findPosition(flipDigit([0, 0, 1, 0], 2))).toEqual(2)
-    expect(findPosition(flipDigit([0, 0, 1, 0], 3))).toEqual(3)
+    expect(findPosition(flipDigit('0000', 0))).toEqual(0)
+    expect(findPosition(flipDigit('0000', 1))).toEqual(1)
+    expect(findPosition(flipDigit('0000', 2))).toEqual(2)
+    expect(findPosition(flipDigit('0000', 3))).toEqual(3)
 
-    expect(findPosition(flipDigit([0, 0, 1, 1], 0))).toEqual(0)
-    expect(findPosition(flipDigit([0, 0, 1, 1], 1))).toEqual(1)
-    expect(findPosition(flipDigit([0, 0, 1, 1], 2))).toEqual(2)
-    expect(findPosition(flipDigit([0, 0, 1, 1], 3))).toEqual(3)
-    expect(findPosition(flipDigit([0, 0, 1, 1], 0))).toEqual(0)
-    expect(findPosition(flipDigit([0, 0, 1, 1], 1))).toEqual(1)
-    expect(findPosition(flipDigit([0, 0, 1, 1], 2))).toEqual(2)
-    expect(findPosition(flipDigit([0, 0, 1, 1], 3))).toEqual(3)
+    expect(findPosition(flipDigit('0001', 0))).toEqual(0)
+    expect(findPosition(flipDigit('0001', 1))).toEqual(1)
+    expect(findPosition(flipDigit('0001', 2))).toEqual(2)
+    expect(findPosition(flipDigit('0001', 3))).toEqual(3)
 
-    expect(findPosition(flipDigit([0, 1, 0, 0], 0))).toEqual(0)
-    expect(findPosition(flipDigit([0, 1, 0, 0], 1))).toEqual(1)
-    expect(findPosition(flipDigit([0, 1, 0, 0], 2))).toEqual(2)
-    expect(findPosition(flipDigit([0, 1, 0, 0], 3))).toEqual(3)
-    expect(findPosition(flipDigit([0, 1, 0, 0], 0))).toEqual(0)
-    expect(findPosition(flipDigit([0, 1, 0, 0], 1))).toEqual(1)
-    expect(findPosition(flipDigit([0, 1, 0, 0], 2))).toEqual(2)
-    expect(findPosition(flipDigit([0, 1, 0, 0], 3))).toEqual(3)
+    expect(findPosition(flipDigit('0010', 0))).toEqual(0)
+    expect(findPosition(flipDigit('0010', 1))).toEqual(1)
+    expect(findPosition(flipDigit('0010', 2))).toEqual(2)
+    expect(findPosition(flipDigit('0010', 3))).toEqual(3)
 
-    expect(findPosition(flipDigit([0, 1, 0, 1], 0))).toEqual(0)
-    expect(findPosition(flipDigit([0, 1, 0, 1], 1))).toEqual(1)
-    expect(findPosition(flipDigit([0, 1, 0, 1], 2))).toEqual(2)
-    expect(findPosition(flipDigit([0, 1, 0, 1], 3))).toEqual(3)
-    expect(findPosition(flipDigit([0, 1, 0, 1], 0))).toEqual(0)
-    expect(findPosition(flipDigit([0, 1, 0, 1], 1))).toEqual(1)
-    expect(findPosition(flipDigit([0, 1, 0, 1], 2))).toEqual(2)
-    expect(findPosition(flipDigit([0, 1, 0, 1], 3))).toEqual(3)
+    expect(findPosition(flipDigit('0011', 0))).toEqual(0)
+    expect(findPosition(flipDigit('0011', 1))).toEqual(1)
+    expect(findPosition(flipDigit('0011', 2))).toEqual(2)
+    expect(findPosition(flipDigit('0011', 3))).toEqual(3)
 
-    expect(findPosition(flipDigit([0, 1, 1, 0], 0))).toEqual(0)
-    expect(findPosition(flipDigit([0, 1, 1, 0], 1))).toEqual(1)
-    expect(findPosition(flipDigit([0, 1, 1, 0], 2))).toEqual(2)
-    expect(findPosition(flipDigit([0, 1, 1, 0], 3))).toEqual(3)
-    expect(findPosition(flipDigit([0, 1, 1, 0], 0))).toEqual(0)
-    expect(findPosition(flipDigit([0, 1, 1, 0], 1))).toEqual(1)
-    expect(findPosition(flipDigit([0, 1, 1, 0], 2))).toEqual(2)
-    expect(findPosition(flipDigit([0, 1, 1, 0], 3))).toEqual(3)
+    expect(findPosition(flipDigit('0100', 0))).toEqual(0)
+    expect(findPosition(flipDigit('0100', 1))).toEqual(1)
+    expect(findPosition(flipDigit('0100', 2))).toEqual(2)
+    expect(findPosition(flipDigit('0100', 3))).toEqual(3)
 
-    expect(findPosition(flipDigit([0, 1, 1, 1], 0))).toEqual(0)
-    expect(findPosition(flipDigit([0, 1, 1, 1], 1))).toEqual(1)
-    expect(findPosition(flipDigit([0, 1, 1, 1], 2))).toEqual(2)
-    expect(findPosition(flipDigit([0, 1, 1, 1], 3))).toEqual(3)
-    expect(findPosition(flipDigit([0, 1, 1, 1], 0))).toEqual(0)
-    expect(findPosition(flipDigit([0, 1, 1, 1], 1))).toEqual(1)
-    expect(findPosition(flipDigit([0, 1, 1, 1], 2))).toEqual(2)
-    expect(findPosition(flipDigit([0, 1, 1, 1], 3))).toEqual(3)
+    expect(findPosition(flipDigit('0101', 0))).toEqual(0)
+    expect(findPosition(flipDigit('0101', 1))).toEqual(1)
+    expect(findPosition(flipDigit('0101', 2))).toEqual(2)
+    expect(findPosition(flipDigit('0101', 3))).toEqual(3)
 
-    expect(findPosition(flipDigit([1, 0, 0, 0], 0))).toEqual(0)
-    expect(findPosition(flipDigit([1, 0, 0, 0], 1))).toEqual(1)
-    expect(findPosition(flipDigit([1, 0, 0, 0], 2))).toEqual(2)
-    expect(findPosition(flipDigit([1, 0, 0, 0], 3))).toEqual(3)
-    expect(findPosition(flipDigit([1, 0, 0, 0], 0))).toEqual(0)
-    expect(findPosition(flipDigit([1, 0, 0, 0], 1))).toEqual(1)
-    expect(findPosition(flipDigit([1, 0, 0, 0], 2))).toEqual(2)
-    expect(findPosition(flipDigit([1, 0, 0, 0], 3))).toEqual(3)
+    expect(findPosition(flipDigit('0110', 0))).toEqual(0)
+    expect(findPosition(flipDigit('0110', 1))).toEqual(1)
+    expect(findPosition(flipDigit('0110', 2))).toEqual(2)
+    expect(findPosition(flipDigit('0110', 3))).toEqual(3)
 
-    expect(findPosition(flipDigit([1, 0, 0, 1], 0))).toEqual(0)
-    expect(findPosition(flipDigit([1, 0, 0, 1], 1))).toEqual(1)
-    expect(findPosition(flipDigit([1, 0, 0, 1], 2))).toEqual(2)
-    expect(findPosition(flipDigit([1, 0, 0, 1], 3))).toEqual(3)
-    expect(findPosition(flipDigit([1, 0, 0, 1], 0))).toEqual(0)
-    expect(findPosition(flipDigit([1, 0, 0, 1], 1))).toEqual(1)
-    expect(findPosition(flipDigit([1, 0, 0, 1], 2))).toEqual(2)
-    expect(findPosition(flipDigit([1, 0, 0, 1], 3))).toEqual(3)
+    expect(findPosition(flipDigit('0111', 0))).toEqual(0)
+    expect(findPosition(flipDigit('0111', 1))).toEqual(1)
+    expect(findPosition(flipDigit('0111', 2))).toEqual(2)
+    expect(findPosition(flipDigit('0111', 3))).toEqual(3)
 
-    expect(findPosition(flipDigit([1, 0, 1, 0], 0))).toEqual(0)
-    expect(findPosition(flipDigit([1, 0, 1, 0], 1))).toEqual(1)
-    expect(findPosition(flipDigit([1, 0, 1, 0], 2))).toEqual(2)
-    expect(findPosition(flipDigit([1, 0, 1, 0], 3))).toEqual(3)
-    expect(findPosition(flipDigit([1, 0, 1, 0], 0))).toEqual(0)
-    expect(findPosition(flipDigit([1, 0, 1, 0], 1))).toEqual(1)
-    expect(findPosition(flipDigit([1, 0, 1, 0], 2))).toEqual(2)
-    expect(findPosition(flipDigit([1, 0, 1, 0], 3))).toEqual(3)
+    expect(findPosition(flipDigit('1000', 0))).toEqual(0)
+    expect(findPosition(flipDigit('1000', 1))).toEqual(1)
+    expect(findPosition(flipDigit('1000', 2))).toEqual(2)
+    expect(findPosition(flipDigit('1000', 3))).toEqual(3)
 
-    expect(findPosition(flipDigit([1, 0, 1, 1], 0))).toEqual(0)
-    expect(findPosition(flipDigit([1, 0, 1, 1], 1))).toEqual(1)
-    expect(findPosition(flipDigit([1, 0, 1, 1], 2))).toEqual(2)
-    expect(findPosition(flipDigit([1, 0, 1, 1], 3))).toEqual(3)
-    expect(findPosition(flipDigit([1, 0, 1, 1], 0))).toEqual(0)
-    expect(findPosition(flipDigit([1, 0, 1, 1], 1))).toEqual(1)
-    expect(findPosition(flipDigit([1, 0, 1, 1], 2))).toEqual(2)
-    expect(findPosition(flipDigit([1, 0, 1, 1], 3))).toEqual(3)
+    expect(findPosition(flipDigit('1001', 0))).toEqual(0)
+    expect(findPosition(flipDigit('1001', 1))).toEqual(1)
+    expect(findPosition(flipDigit('1001', 2))).toEqual(2)
+    expect(findPosition(flipDigit('1001', 3))).toEqual(3)
 
-    expect(findPosition(flipDigit([1, 1, 0, 0], 0))).toEqual(0)
-    expect(findPosition(flipDigit([1, 1, 0, 0], 1))).toEqual(1)
-    expect(findPosition(flipDigit([1, 1, 0, 0], 2))).toEqual(2)
-    expect(findPosition(flipDigit([1, 1, 0, 0], 3))).toEqual(3)
-    expect(findPosition(flipDigit([1, 1, 0, 0], 0))).toEqual(0)
-    expect(findPosition(flipDigit([1, 1, 0, 0], 1))).toEqual(1)
-    expect(findPosition(flipDigit([1, 1, 0, 0], 2))).toEqual(2)
-    expect(findPosition(flipDigit([1, 1, 0, 0], 3))).toEqual(3)
+    expect(findPosition(flipDigit('1010', 0))).toEqual(0)
+    expect(findPosition(flipDigit('1010', 1))).toEqual(1)
+    expect(findPosition(flipDigit('1010', 2))).toEqual(2)
+    expect(findPosition(flipDigit('1010', 3))).toEqual(3)
 
-    expect(findPosition(flipDigit([1, 1, 0, 1], 0))).toEqual(0)
-    expect(findPosition(flipDigit([1, 1, 0, 1], 1))).toEqual(1)
-    expect(findPosition(flipDigit([1, 1, 0, 1], 2))).toEqual(2)
-    expect(findPosition(flipDigit([1, 1, 0, 1], 3))).toEqual(3)
-    expect(findPosition(flipDigit([1, 1, 0, 1], 0))).toEqual(0)
-    expect(findPosition(flipDigit([1, 1, 0, 1], 1))).toEqual(1)
-    expect(findPosition(flipDigit([1, 1, 0, 1], 2))).toEqual(2)
-    expect(findPosition(flipDigit([1, 1, 0, 1], 3))).toEqual(3)
+    expect(findPosition(flipDigit('1011', 0))).toEqual(0)
+    expect(findPosition(flipDigit('1011', 1))).toEqual(1)
+    expect(findPosition(flipDigit('1011', 2))).toEqual(2)
+    expect(findPosition(flipDigit('1011', 3))).toEqual(3)
 
-    expect(findPosition(flipDigit([1, 1, 1, 0], 0))).toEqual(0)
-    expect(findPosition(flipDigit([1, 1, 1, 0], 1))).toEqual(1)
-    expect(findPosition(flipDigit([1, 1, 1, 0], 2))).toEqual(2)
-    expect(findPosition(flipDigit([1, 1, 1, 0], 3))).toEqual(3)
-    expect(findPosition(flipDigit([1, 1, 1, 0], 0))).toEqual(0)
-    expect(findPosition(flipDigit([1, 1, 1, 0], 1))).toEqual(1)
-    expect(findPosition(flipDigit([1, 1, 1, 0], 2))).toEqual(2)
-    expect(findPosition(flipDigit([1, 1, 1, 0], 3))).toEqual(3)
+    expect(findPosition(flipDigit('1100', 0))).toEqual(0)
+    expect(findPosition(flipDigit('1100', 1))).toEqual(1)
+    expect(findPosition(flipDigit('1100', 2))).toEqual(2)
+    expect(findPosition(flipDigit('1100', 3))).toEqual(3)
 
-    expect(findPosition(flipDigit([1, 1, 1, 1], 0))).toEqual(0)
-    expect(findPosition(flipDigit([1, 1, 1, 1], 1))).toEqual(1)
-    expect(findPosition(flipDigit([1, 1, 1, 1], 2))).toEqual(2)
-    expect(findPosition(flipDigit([1, 1, 1, 1], 3))).toEqual(3)
-    expect(findPosition(flipDigit([1, 1, 1, 1], 0))).toEqual(0)
-    expect(findPosition(flipDigit([1, 1, 1, 1], 1))).toEqual(1)
-    expect(findPosition(flipDigit([1, 1, 1, 1], 2))).toEqual(2)
-    expect(findPosition(flipDigit([1, 1, 1, 1], 3))).toEqual(3)
+    expect(findPosition(flipDigit('1101', 0))).toEqual(0)
+    expect(findPosition(flipDigit('1101', 1))).toEqual(1)
+    expect(findPosition(flipDigit('1101', 2))).toEqual(2)
+    expect(findPosition(flipDigit('1101', 3))).toEqual(3)
+
+    expect(findPosition(flipDigit('1110', 0))).toEqual(0)
+    expect(findPosition(flipDigit('1110', 1))).toEqual(1)
+    expect(findPosition(flipDigit('1110', 2))).toEqual(2)
+    expect(findPosition(flipDigit('1110', 3))).toEqual(3)
+
+    expect(findPosition(flipDigit('1111', 0))).toEqual(0)
+    expect(findPosition(flipDigit('1111', 1))).toEqual(1)
+    expect(findPosition(flipDigit('1111', 2))).toEqual(2)
+    expect(findPosition(flipDigit('1111', 3))).toEqual(3)
   })
 })
